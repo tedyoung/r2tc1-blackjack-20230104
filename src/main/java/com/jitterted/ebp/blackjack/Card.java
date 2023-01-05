eae73efb0c1b8f808a5a628e34cbd8747c5f5534
Ted M. Young <tedyoung@gmail.com>
2023-01-05 11:22:00 -0800

    LAB #4: Tidy up: removed unused constructor and methods ("scaffolding")

--- src/main/java/com/jitterted/ebp/blackjack/Card.java
+++ src/main/java/com/jitterted/ebp/blackjack/Card.java
@@ -8,14 +8,6 @@ public class Card {
     private final Suit suit;
     private final String rank;
 
-    // GOAL: Suit suit, String rank
-    @Deprecated
-    public Card(String symbol, String rank) {
-        this.suit = Suit.from(symbol);
-        this.rank = rank;
-    }
-
-    // KEEP
     public Card(Suit suit, String rank) {
         this.suit = suit;
         this.rank = rank;
--- src/main/java/com/jitterted/ebp/blackjack/Suit.java
+++ src/main/java/com/jitterted/ebp/blackjack/Suit.java
@@ -1,30 +1,15 @@
 package com.jitterted.ebp.blackjack;
 
-import java.util.List;
-
 public enum Suit {
     SPADES("♠"),
     DIAMONDS("♦"),
     HEARTS("♥"),
     CLUBS("♣");
 
-    @Deprecated
-    static final List<String> SUITS = List.of("♠", "♦", "♥", "♣");
     private final String symbol;
 
     Suit(String symbol) {
         this.symbol = symbol;
-    }
-
-    // SCAFFOLDING
-    @Deprecated
-    public static Suit from(String symbol) {
-        for (Suit suit : values()) {
-            if (symbol.equals(suit.symbol())) {
-                return suit;
-            }
-        }
-        return null;
     }
 
     public String symbol() {

a7133349220d26c8bb205051c98f20e5ed3567e4
Ted M. Young <tedyoung@gmail.com>
2023-01-05 11:07:44 -0800

    LAB #4: Using Suit enum internally in Card, all tests pass

---
+++ src/main/java/com/jitterted/ebp/blackjack/Suit.java
@@ -0,0 +1,32 @@
+package com.jitterted.ebp.blackjack;
+
+import java.util.List;
+
+public enum Suit {
+    SPADES("♠"),
+    DIAMONDS("♦"),
+    HEARTS("♥"),
+    CLUBS("♣");
+
+    static final List<String> SUITS = List.of("♠", "♦", "♥", "♣");
+    private final String symbol;
+
+    Suit(String symbol) {
+        this.symbol = symbol;
+    }
+
+    // SCAFFOLDING
+    @Deprecated
+    public static Suit from(String symbol) {
+        for (Suit suit : values()) {
+            if (symbol.equals(suit.symbol())) {
+                return suit;
+            }
+        }
+        return null;
+    }
+
+    public String symbol() {
+        return symbol;
+    }
+}

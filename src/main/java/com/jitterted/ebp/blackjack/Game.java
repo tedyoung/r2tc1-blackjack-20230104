9cde22b4868492fa7b55728c9a1bdc7fd6947ca3
Ted M. Young <tedyoung@gmail.com>
2023-01-11 10:35:49 -0800

    LAB #5: Extracted new Hand class, with getCards() as public method to access List<Card>.

--- src/main/java/com/jitterted/ebp/blackjack/Game.java
+++ src/main/java/com/jitterted/ebp/blackjack/Game.java
@@ -3,7 +3,6 @@ package com.jitterted.ebp.blackjack;
 import org.fusesource.jansi.Ansi;
 import org.fusesource.jansi.AnsiConsole;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
@@ -14,8 +13,8 @@ public class Game {
 
     private final Deck deck;
 
-    private final List<Card> dealerHand = new ArrayList<>();
-    private final List<Card> playerHand = new ArrayList<>();
+    private final Hand playerHand = new Hand();
+    private final Hand dealerHand = new Hand();
 
     public static void main(String[] args) {
 
@@ -72,11 +71,11 @@ public class Game {
     }
 
     private void dealCardToDealer() {
-        dealerHand.add(deck.draw());
+        dealerHand.getCards().add(deck.draw());
     }
 
     private void dealCardToPlayer() {
-        playerHand.add(deck.draw());
+        playerHand.getCards().add(deck.draw());
     }
 
     public void play() {
@@ -92,11 +91,11 @@ public class Game {
     private void displayOutcome(boolean playerBusted) {
         if (playerBusted) {
             System.out.println("You Busted, so you lose.  💸");
-        } else if (handValueOf(dealerHand) > 21) {
+        } else if (handValueOf(dealerHand.getCards()) > 21) {
             System.out.println("Dealer went BUST, Player wins! Yay for you!! 💵");
-        } else if (handValueOf(dealerHand) < handValueOf(playerHand)) {
+        } else if (handValueOf(dealerHand.getCards()) < handValueOf(playerHand.getCards())) {
             System.out.println("You beat the Dealer! 💵");
-        } else if (handValueOf(dealerHand) == handValueOf(playerHand)) {
+        } else if (handValueOf(dealerHand.getCards()) == handValueOf(playerHand.getCards())) {
             System.out.println("Push: You tie with the Dealer. 💸");
         } else {
             System.out.println("You lost to the Dealer. 💸");
@@ -106,7 +105,7 @@ public class Game {
     private void dealerTurn(boolean playerBusted) {
         // Dealer makes its choice automatically based on a simple heuristic (<=16, hit, 17>=stand)
         if (!playerBusted) {
-            while (handValueOf(dealerHand) <= 16) {
+            while (handValueOf(dealerHand.getCards()) <= 16) {
                 dealCardToDealer();
             }
         }
@@ -123,7 +122,7 @@ public class Game {
             }
             if (playerChoseHit(playerChoice)) {
                 dealCardToPlayer();
-                if (handValueOf(playerHand) > 21) {
+                if (handValueOf(playerHand.getCards()) > 21) {
                     playerBusted = true;
                 }
             } else {
@@ -169,27 +168,27 @@ public class Game {
     private void displayGameState() {
         System.out.print(ansi().eraseScreen().cursor(1, 1));
         System.out.println("Dealer has: ");
-        System.out.println(dealerHand.get(0).display()); // first card is Face Up
+        System.out.println(dealerHand.getCards().get(0).display()); // first card is Face Up
 
         // second card is the hole card, which is hidden
         displayBackOfCard();
 
         System.out.println();
         System.out.println("Player has: ");
-        displayHand(playerHand);
-        System.out.println(" (" + handValueOf(playerHand) + ")");
+        displayHand(playerHand.getCards());
+        System.out.println(" (" + handValueOf(playerHand.getCards()) + ")");
     }
 
     private void displayFinalGameState() {
         System.out.print(ansi().eraseScreen().cursor(1, 1));
         System.out.println("Dealer has: ");
-        displayHand(dealerHand);
-        System.out.println(" (" + handValueOf(dealerHand) + ")");
+        displayHand(dealerHand.getCards());
+        System.out.println(" (" + handValueOf(dealerHand.getCards()) + ")");
 
         System.out.println();
         System.out.println("Player has: ");
-        displayHand(playerHand);
-        System.out.println(" (" + handValueOf(playerHand) + ")");
+        displayHand(playerHand.getCards());
+        System.out.println(" (" + handValueOf(playerHand.getCards()) + ")");
     }
 
     private void displayBackOfCard() {
---
+++ src/main/java/com/jitterted/ebp/blackjack/Hand.java
@@ -0,0 +1,13 @@
+package com.jitterted.ebp.blackjack;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Hand {
+    private final List<Card> cards = new ArrayList<Card>();
+
+    @Deprecated
+    public List<Card> getCards() {
+        return cards;
+    }
+}

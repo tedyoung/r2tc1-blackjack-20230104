0414544c97ea84df4493df4fd4f81b445f90ba09
Ted M. Young <tedyoung@gmail.com>
2023-01-12 11:00:15 -0800

    TDD: R - removed duplicate information represented by isEmpty

--- src/main/java/com/jitterted/ebp/blackjack/Wallet.java
+++ src/main/java/com/jitterted/ebp/blackjack/Wallet.java
@@ -2,23 +2,21 @@ package com.jitterted.ebp.blackjack;
 
 public class Wallet {
 
-    private boolean isEmpty;
     private int balance = 0;
 
     public Wallet() {
-        isEmpty = true;
     }
 
     public boolean isEmpty() {
-        return isEmpty;
+        return balance == 0;
     }
 
     public void addMoney(int amount) {
-        isEmpty = false;
         balance += amount;
     }
 
     public int balance() {
         return balance;
     }
+
 }
